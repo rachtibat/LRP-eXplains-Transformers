37f19765814c0f0ef0f6146900ea65bfa0efc9a3
rachtibat <r.achtibat@outlook.de>
2025-03-15 14:39:38 +0100

    fix wrong rule at addmm because it's not bi-linear

--- lxt/models/gpt2.py
+++ lxt/models/gpt2.py
@@ -85,7 +85,7 @@ class Conv1D(nn.Module):
     def forward(self, x):
         size_out = x.size()[:-1] + (self.nf,)
         #x = torch.addmm(self.bias, x.view(-1, x.size(-1)), self.weight)
-        x = lf.add2(self.bias, lf.matmul(x.view(-1, x.size(-1)), self.weight))
+        x = lf.add2(self.bias, lf.linear_epsilon(x.view(-1, x.size(-1)), self.weight.T))
         x = x.view(size_out)
         return x
     
