01b7a86824f2ab6389c4f237f22b53ac089cb480
rachtibat <r.achtibat@outlook.de>
2025-07-10 17:16:34 +0200

    add gemma 3 and qwen 3

---
+++ examples/quantized_qwen3.py
@@ -0,0 +1,59 @@
+import torch
+from transformers import AutoTokenizer
+from transformers.models.qwen3 import modeling_qwen3
+from transformers import BitsAndBytesConfig
+
+from lxt.efficient import monkey_patch
+from lxt.utils import pdf_heatmap, clean_tokens
+
+# modify the Qwen2 module to compute LRP in the backward pass
+monkey_patch(modeling_qwen3, verbose=True)
+
+# optional 4bit quantization 
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.bfloat16, # use bfloat16 to prevent overflow in gradients
+)
+
+path = 'Qwen/Qwen3-1.7B'
+model = modeling_qwen3.Qwen3ForCausalLM.from_pretrained(path, device_map='cuda', torch_dtype=torch.bfloat16, quantization_config=quantization_config)
+
+# optional gradient checkpointing to save memory (2x forward pass)
+model.train()
+model.gradient_checkpointing_enable()
+
+# deactive gradients on parameters to save memory
+for param in model.parameters():
+    param.requires_grad = False
+
+tokenizer = AutoTokenizer.from_pretrained(path)
+
+prompt = """Context: Mount Everest attracts many climbers, including highly experienced mountaineers. There are two main climbing routes, one approaching the summit from the southeast in Nepal (known as the standard route) and the other from the north in Tibet. While not posing substantial technical climbing challenges on the standard route, Everest presents dangers such as altitude sickness, weather, and wind, as well as hazards from avalanches and the Khumbu Icefall. As of November 2022, 310 people have died on Everest. Over 200 bodies remain on the mountain and have not been removed due to the dangerous conditions. The first recorded efforts to reach Everest's summit were made by British mountaineers. As Nepal did not allow foreigners to enter the country at the time, the British made several attempts on the north ridge route from the Tibetan side. After the first reconnaissance expedition by the British in 1921 reached 7,000 m (22,970 ft) on the North Col, the 1922 expedition pushed the north ridge route up to 8,320 m (27,300 ft), marking the first time a human had climbed above 8,000 m (26,247 ft). The 1924 expedition resulted in one of the greatest mysteries on Everest to this day: George Mallory and Andrew Irvine made a final summit attempt on 8 June but never returned, sparking debate as to whether they were the first to reach the top. Tenzing Norgay and Edmund Hillary made the first documented ascent of Everest in 1953, using the southeast ridge route. Norgay had reached 8,595 m (28,199 ft) the previous year as a member of the 1952 Swiss expedition. The Chinese mountaineering team of Wang Fuzhou, Gonpo, and Qu Yinhua made the first reported ascent of the peak from the north ridge on 25 May 1960. \
+Question: How high did they climb in 1922? According to the text, the 1922 expedition reached 8,"""
+
+# get input embeddings so that we can compute gradients w.r.t. input embeddings
+input_ids = tokenizer(prompt, return_tensors="pt", add_special_tokens=True).input_ids.to(model.device)
+input_embeds = model.get_input_embeddings()(input_ids)
+
+# inference and get the maximum logit at the last position (we can also explain other tokens)
+output_logits = model(inputs_embeds=input_embeds.requires_grad_(), use_cache=False).logits
+max_logits, max_indices = torch.max(output_logits[0, -1, :], dim=-1)
+
+# Backward pass (the relevance is initialized with the value of max_logits)
+# This initiates the LRP computation through the network
+max_logits.backward()
+
+# obtain relevance by computing Input * Gradient
+relevance = (input_embeds * input_embeds.grad).float().sum(-1).detach().cpu()[0] # cast to float32 before summation for higher precision
+
+# normalize relevance between [-1, 1] for plotting
+relevance = relevance / relevance.abs().max()
+
+# remove special characters from token strings and plot the heatmap
+tokens = tokenizer.convert_ids_to_tokens(input_ids[0])
+tokens = clean_tokens(tokens)
+
+pdf_heatmap(tokens, relevance, path='qwen3_1.7B_heatmap.pdf', backend='xelatex') # backend='xelatex' supports more characters
+
+# plot again without first token, because it receives large relevance values overshadowing the rest
+pdf_heatmap(tokens[1:], relevance[1:] / relevance[1:].max(), path='qwen3_1.7B_heatmap_wo_first.pdf', backend='xelatex')
