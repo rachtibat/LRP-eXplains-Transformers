8e4bb9a0a9f3ef4bbf47e692e175e53677829945
Andrew Boldi <andrewjboldi@gmail.com>
2025-07-18 18:30:37 -0700

    more file formatting

--- lxt/efficient/models/gemma3.py
+++ lxt/efficient/models/gemma3.py
@@ -5,11 +5,17 @@ from transformers.models.gemma3 import modeling_gemma3
 from transformers.models.gemma3.modeling_gemma3 import Gemma3MLP, Gemma3RMSNorm
 
 from lxt.efficient.patches import patch_method, patch_attention, patch_cp_attention
-from lxt.efficient.patches import gated_mlp_forward, cp_gated_mlp_forward, dropout_forward
+from lxt.efficient.patches import (
+    gated_mlp_forward,
+    cp_gated_mlp_forward,
+    dropout_forward,
+)
 from lxt.efficient.rules import stop_gradient
+
 
 def gemma3_norm(self, x):
     return x * stop_gradient(torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps))
+
 
 attnLRP = {
     Gemma3MLP: partial(patch_method, gated_mlp_forward),
